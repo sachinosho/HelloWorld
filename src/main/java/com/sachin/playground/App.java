50f5af76c7f49234fadab085bd21d880e9a836d7
Sachin S <sachinjul27>
2019-01-10 17:04:53 -0800

    First commit

---
+++ src/main/java/com/sachin/playground/App.java
@@ -0,0 +1,28 @@
+package com.sachin.playground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String greet = "Hello World1";    
+    public static Integer address = 67;
+
+    public static void main( String[] args )
+    {
+        System.out.println( greet);
+    }
+    public address()
+    {
+        System.out.println( address);
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( greet);
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( greet);
+    }
+}
